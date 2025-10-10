75c15722311f9c36bbd2fa41172e9b1c1f0f6157
Tamiris Coutinho <tami_coliveira@hotmail.com>
2025-10-10 16:07:21 -0300

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # script.sh
 
-echo "=== Resultado do Script ===" > resultado.txt
-echo "Data de execução: $(date '+%Y-%m-%d %H:%M:%S')" >> resultado.txt
-echo "Hostname: $(hostname)" >> resultado.txt
-echo "Usuário atual: $(whoami)" >> resultado.txt
-echo "Diretório atual: $(pwd)" >> resultado.txt
-echo "-----------------------------" >> resultado.txt
+echo "=== Resultado do Script ===" > resultado.csv
+echo "Data de execução: $(date '+%Y-%m-%d %H:%M:%S')" >> resultado.csv
+echo "Hostname: $(hostname)" >> resultado.csv
+echo "Usuário atual: $(whoami)" >> resultado.csv
+echo "Diretório atual: $(pwd)" >> resultado.csv
+echo "-----------------------------" >> resultado.csv
 
 echo "Arquivo resultado.txt criado com sucesso!"
